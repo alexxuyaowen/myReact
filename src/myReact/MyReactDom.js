f981f9820e48a8b2ff5f26f6e57059f4a21ae95f
Yaowen Xu <x@Macintosh.local>
2022-06-15 11:36:33 -0700

    +

--- src/myReact/MyReactDom.js
+++ src/myReact/MyReactDom.js
@@ -1,81 +1,77 @@
 import React from 'react';
 
-const render = (
-    reactElement,
-    domElement
-) => {
-    let curDom;
-    console.log("TEST", reactElement)
-    if (reactElement === undefined) {
-        return;
+const render = (reactElement, domElement) => {
+  let curDom;
+  console.log('TEST', reactElement);
+  if (reactElement === undefined) {
+    return;
+  }
+  if ((typeof reactElement === 'string') | (typeof reactElement === 'number')) {
+    curDom = document.createTextNode(reactElement);
+  } else {
+    const { type, props } = reactElement;
+    /// if type is ClassCompoennt
+    if (type.prototype instanceof React.Component) {
+      console.log('class componnent props', props);
+      /// Mounting
+      /// constructor
+      const curInstance = new type(props);
+      console.log('curInstance', curInstance);
+
+      // getDerivedStateFromProps
+      curInstance.state = type.getDerivedStateFromProps(
+        props,
+        curInstance.state
+      );
+      console.log('curInstance', curInstance);
+
+      // render
+      const curReactElement = curInstance.render();
+      console.log('curReactElement', curReactElement);
+      render(curReactElement, domElement);
+      if (curInstance.componentDidmount) {
+        curInstance.componentDidmount();
+      }
+      return;
     }
-    if (typeof reactElement === 'string' | typeof reactElement === 'number') {
-        curDom =
-            document.createTextNode(reactElement);
-    } else {
-        const { type, props } = reactElement;
-        /// if type is ClassCompoennt
-        if (type.prototype instanceof React.Component) {
-            console.log('class componnent props', props)
-            /// Mounting
-            /// constructor
-            const curInstance = new type(props);
-            console.log("curInstance", curInstance)
 
-            // getDerivedStateFromProps
-            curInstance.state = type.getDerivedStateFromProps(props, curInstance.state)
-            console.log("curInstance", curInstance)
+    // Assignment if it is function component
+    if (typeof type === 'function') {
+      render(new type(props), domElement);
+      return;
+    }
 
-            // render
-            const curReactElement = curInstance.render();
-            console.log("curReactElement", curReactElement);
-            render(curReactElement, domElement);
-            if (curInstance.componentDidmount) {
-                curInstance.componentDidmount()
-            }
-            return
+    /// else
+    curDom = document.createElement(type);
+    Object.entries(props).forEach(([key, value]) => {
+      if (key === 'children') {
+        if (Array.isArray(value)) {
+          console.log(value);
+          value.forEach(rElement => {
+            render(rElement, curDom);
+          });
+        } else {
+          render(value, curDom);
         }
+      } else if (key.startsWith('on')) {
+        curDom.addEventListener(getEventActionFromProps(key), value);
+      } else {
+        curDom[key] = value;
+      }
+    });
+  }
 
-        // Assignment if it is function component
-
-        /// else 
-        curDom = document.createElement(type);
-        Object.entries(props).forEach(
-            ([key, value]) => {
-                if (key === 'children') {
-                    if (Array.isArray(value)) {
-                        console.log(value);
-                        value.forEach((rElement) => {
-                            render(rElement, curDom);
-                        });
-                    } else {
-                        render(value, curDom);
-                    }
-                } else if (key.startsWith('on')) {
-                    curDom.addEventListener(
-                        getEventActionFromProps(key),
-                        value
-                    );
-                } else {
-                    curDom[key] = value;
-                }
-            }
-        );
-    }
-
-    domElement.appendChild(curDom);
+  domElement.appendChild(curDom);
 };
-
 
 //utils
 
 function getEventActionFromProps(propsKey) {
-    return propsKey.slice(2).toLowerCase();
+  return propsKey.slice(2).toLowerCase();
 }
 
 const MyReactDOM = {
-    render: render
-}
+  render: render,
+};
 
-
-export default MyReactDOM
+export default MyReactDOM;
